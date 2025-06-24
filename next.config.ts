c0d566f2cf1d9add9ade57127b8a3ee91cefc1c4
Keizer27 <akinpeluusman2018@gmail.com>
2025-06-24 14:52:38 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,28 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+    // Allow data URIs
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
+
+export default nextConfig;
