69f44c51f2c00df5f44545d694f4348237d480bc
Keizer27 <akinpeluusman2018@gmail.com>
2025-06-25 01:49:04 +0100

    Update firebase.js

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
+  apiKey: "AIzaSyAawGhoA-BLIHNlobk_F3F9eflMaV8yPOw",
   authDomain: "keizer-ai.firebaseapp.com",
   projectId: "keizer-ai",
   storageBucket: "keizer-ai.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  messagingSenderId: "keizer-ai-suite",
+  appId: "634906926507"
 };
 
 const app = initializeApp(firebaseConfig);
